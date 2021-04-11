e159d9098c008022445d604d734f856c67952794
ThanhTT <umbalaconmeogia@gmail.com>
2021-04-11 23:57:33 +0900

    Add yii2 original LoginForm.php

--- src/models/SystemUser.php
+++ src/models/SystemUser.php
@@ -3,6 +3,9 @@ namespace umbalaconmeogia\systemuser\models;
 
 /**
  * Wrapper of User class.
+ *
+ * Because User is copied from yii2 source code, we try to keep it as original one.
+ * All additional code will be added to SystemUser.
  */
 class SystemUser extends User
 {
